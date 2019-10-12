e191d1aa991597d3393fb656750cb729596013fd
jinalshah999 <jinalshah999@gmail.com>
2019-10-13 01:37:05 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DragDropModule } from "@angular/cdk/drag-drop";
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragdropdemoComponent } from './dragdropdemo/dragdropdemo.component';
+import { MaindragdropComponent } from './maindragdrop/maindragdrop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragdropdemoComponent,
+    MaindragdropComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
